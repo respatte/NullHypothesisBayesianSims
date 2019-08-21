6805ff232a472ca776fd1d13a7b4d18fc028fca8
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2019-08-21 16:39:00 +0100

    b-value_p-value plot generated, fun stuff

--- src/BayesFactors.R
+++ src/BayesFactors.R
@@ -1,13 +1,15 @@
-# LIBRARY IMPORTS =======================================================================
+# LIBRARY IMPORTS ==================================================================================
 library(lme4)
 library(emmeans)
 library(brms)
 library(tidyverse)
+library(scales)
 library(future)
 library(future.apply)
 plan(multiprocess)
 
-# BAYESIAN POINT NULL HYPOTHESIS TESTING ================================================
+# BAYESIAN POINT NULL HYPOTHESIS TESTING ===========================================================
+# Define simulation function
 H_naught.test <- function(N){
   # Generate random data
   # mean=0 for null, sd=.5 from real data
@@ -21,45 +23,48 @@ H_naught.test <- function(N){
   
   # Define STB analysis function, returning emmeans analysis
   stb.analysis <- function(df.list){
-    e <- future_lapply(df.list,
-                       function(df){
-                         m <- lmer(ChanceArcsin ~ ContrastType*Condition +
-                                     (1 | Participant),
-                                   data = df)
-                         t <- emmeans(m, ~ ContrastType | Condition,
-                                      options = list(infer = c(T, T), null = 0,
-                                                     level = .95)) %>%
-                           as_tibble()
-                         return(t)
-                       }) %>%
-      bind_rows()
-    return(e)
+    future_lapply(seq_along(df.list),
+                  function(i){
+                    lmer(ChanceArcsin ~ ContrastType*Condition +
+                           (1 | Participant),
+                         data = df.list[[i]]) %>%
+                      emmeans(~ ContrastType | Condition,
+                              options = list(infer = c(T, T), null = 0,
+                                             level = .95)) %>%
+                      as_tibble() %>%
+                      mutate(Sim = i) %>%
+                      return()
+                  }) %>%
+      bind_rows() %>%
+      return()
   }
   
   # Define Bayesian analysis function, returning hyp. test bf
   bayesian.analysis <- function(df.list){
     p <- c(set_prior("normal(0,.5)", class = "Intercept"),
            set_prior("normal(0,.5)", class = "b"))
-    bf <- future_lapply(df.list,
-                        function(df){
-                          bf <- brm(ChanceArcsin ~ ContrastType + Condition +
-                                      ContrastType:Condition +
-                                      (1 | Participant),
-                                    data = df, prior = p, family = gaussian(),
-                                    chains = 4, iter = 2000,
-                                    save_all_pars = T) %>%
-                            hypothesis(c("Intercept > 0",
-                                         "Intercept + ContrastTypeTail > 0",
-                                         "Intercept + ConditionNo Label > 0",
-                                         paste("Intercept +",
-                                               "ConditionNo Label +",
-                                               "ContrastTypeTail +",
-                                               "ContrastTypeTail:ConditionNo Label",
-                                               "> 0")))
-                          return(as_tibble(bf$hypothesis))
-                        }) %>%
-      bind_rows()
-    return(bf)
+    future_lapply(seq_along(df.list),
+                  function(i){
+                    bf <- brm(ChanceArcsin ~ ContrastType + Condition +
+                                ContrastType:Condition +
+                                (1 | Participant),
+                              data = df.list[[i]], prior = p, family = gaussian(),
+                              chains = 4, iter = 2000,
+                              save_all_pars = T) %>%
+                      hypothesis(c("Intercept > 0",
+                                   "Intercept + ContrastTypeTail > 0",
+                                   "Intercept + ConditionNo Label > 0",
+                                   paste("Intercept +",
+                                         "ConditionNo Label +",
+                                         "ContrastTypeTail +",
+                                         "ContrastTypeTail:ConditionNo Label",
+                                         "> 0")))
+                    bf$hypothesis %>% as_tibble() %>%
+                      mutate(Sim = i) %>%
+                      return()
+                  }) %>%
+      bind_rows() %>%
+      return()
   }
   
   # Get evidence summary from simulations
@@ -79,8 +84,58 @@ H_naught.test <- function(N){
               SampleTheoryTime = stb.time))
 }
 
-new_old.sims.results.48 <- H_naught.test(N = 48)
-bayesian.95 <- quantile(new_old.sims.results.200$BayesianEvidence$Evid.Ratio, .95)
-stb.95 <- quantile(new_old.sims.results.200$SampleTheoryEvidence$p.value, .05)
-bayesian.above3 <- sum(new_old.sims.results.200$BayesianEvidence$Evid.Ratio > 3) / 400
-stb.bellow_dot05 <- sum(new_old.sims.results.200$SampleTheoryEvidence$p.value<.05) / 400
+# Get simulations (takes about one hour on 2.3GHz a quadcore with 16G RAM + swap)
+run_sims <- F
+if(run_sims){
+  new_old.sims.results.48 <- H_naught.test(N = 48)
+  saveRDS(new_old.sims.results.48, file = "../results/NewOld_48.rds")
+}else{
+  new_old.sims.results.48 <- readRDS("../results/NewOld_48.rds")
+}
+
+# Get quantiles for STB and Bayesian stats
+bayesian.95 <- quantile(new_old.sims.results.48$BayesianEvidence$Evid.Ratio, .95)
+stb.95 <- quantile(new_old.sims.results.48$SampleTheoryEvidence$p.value, .05)
+bayesian.above3 <- sum(new_old.sims.results.48$BayesianEvidence$Evid.Ratio > 3) / 400
+stb.bellow_dot05 <- sum(new_old.sims.results.48$SampleTheoryEvidence$p.value<.05) / 400
+
+# Combine data for plot and analysis
+bayesian.evidence <- new_old.sims.results.48$BayesianEvidence %>%
+  select(c(Sim, Evid.Ratio, Condition, ContrastType, Hypothesis)) %>%
+  mutate(Hypothesis = case_when(grepl("ContrastTypeTail:ConditionNoLabel",
+                                      Hypothesis) ~ "(Interaction)",
+                                grepl("ContrastTypeTail", Hypothesis) ~ "ContrastType",
+                                grepl("ConditionNoLabel", Hypothesis) ~ "Condition",
+                                T ~ "(Intercept)"),
+         Hypothesis = parse_factor(Hypothesis, levels = c("(Intercept)",
+                                                          "ContrastType",
+                                                          "Condition",
+                                                          "(Interaction)")))
+stb.evidence <- new_old.sims.results.48$SampleTheoryEvidence %>%
+  select(c(Sim, p.value, Condition, ContrastType))
+global.evidence <- full_join(bayesian.evidence, stb.evidence)
+
+# Plot b-values over p-values
+generate_plots <- F
+if(generate_plots){
+  b_to_p.plot <- global.evidence %>%
+    ggplot(aes(x = p.value,
+               y = Evid.Ratio,
+               colour = Hypothesis)) +
+    scale_y_continuous(trans = log10_trans()) +
+    ylab("Bayes Factor") + xlab("p-value") + theme_bw() +
+    theme(legend.position = "top",
+          legend.title = element_blank()) +
+    geom_point(alpha = .5) +
+    geom_vline(xintercept = .05, colour = "black",
+               linetype = "62", alpha = .5) +
+    scale_colour_brewer(palette = "Dark2",
+                        labels = c("Intercept",
+                                   "Main Effect 1",
+                                   "Main Effect 2",
+                                   "Interaction"))
+  ggsave("../results/b-value_p-value.pdf",
+         plot = b_to_p.plot,
+         width = 5, height = 5,
+         dpi = 600)
+}
